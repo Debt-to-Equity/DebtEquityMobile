0bc3436c970b2555b13456e8f2d49b633f031e03
coopergoldenholt <coopergoldenholt12@gmail.com>
2022-02-21 21:32:32 -0700

    added svg

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -17,7 +17,7 @@ import NotFoundScreen from '../../screens/NotFoundScreen';
 import TabOneScreen from '../../screens/TabOneScreen';
 import TabTwoScreen from '../../screens/TabTwoScreen';
 import AgentDashboard from '../views/AgentDashboard/AgentDashboard';
-// import CustomerDashboard from '../views/CustomerDashboard';
+import CustomerDashboard from '../views/CustomerDashboard/CustomerDashboard';
 import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../../types';
 import LinkingConfiguration from './LinkingConfiguration';
 import Wizard from '../views/Wizard/Wizard';
@@ -66,28 +66,14 @@ function BottomTabNavigator() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme].tint,
       }}>
-      {/* <BottomTab.Screen
+      <BottomTab.Screen
         name="Customer"
         component={CustomerDashboard}
         options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
           title: 'Tab One',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Pressable
-              onPress={() => navigation.navigate('Modal')}
-              style={({ pressed }) => ({
-                opacity: pressed ? 0.5 : 1,
-              })}>
-              <FontAwesome
-                name="info-circle"
-                size={25}
-                color={Colors[colorScheme].text}
-                style={{ marginRight: 15 }}
-              />
-            </Pressable>
-          ),
         })}
-      /> */}
+      />
       <BottomTab.Screen
         name="Agent"
         component={AgentDashboard}
