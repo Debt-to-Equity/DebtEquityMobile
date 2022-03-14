d5a6a4a4e00a5d6c7bf4c7bd3d81719cf34282c3
Cooper Holt <cooperholt@Coopers-MacBook-Pro.local>
2022-03-14 17:28:06 -0600

    updated register

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,65 +1,80 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState } from "react";
+import { View, TouchableOpacity, SafeAreaView } from "react-native";
+
 import {
-  View,
-  TouchableOpacity,
-  SafeAreaView
-} from 'react-native';
+  TextInput,
+  HelperText,
+  Text,
+  Button,
+  useTheme,
+} from "react-native-paper";
 
-import { TextInput, HelperText, Text, Button, useTheme } from 'react-native-paper';
-
-import { Colors } from '../../styles';
-import { BrightText, MediumText } from '../../components/Texts'
-import { useForm } from '../../hooks/useForm';
-import { loginUser } from '../../api/loginUser';
-import { useError } from '../../hooks/useError';
-import { useUser } from '../../hooks/useUser';
-import { UserContext } from '../../context/UserContext';
+import { Colors } from "../../styles";
+import { BrightText, MediumText } from "../../components/Texts";
+import { useForm } from "../../hooks/useForm";
+import { loginUser } from "../../api/loginUser";
+import { useError } from "../../hooks/useError";
+import { UserContext } from "../../context/UserContext";
 
 interface IProps {
   navigation: any;
 }
 
-const Login: React.FC<IProps> = ({navigation}) => {
-  const {user, saveUser} = useContext(UserContext);
+const Login: React.FC<IProps> = ({ navigation }) => {
+  const { user, saveUser } = useContext(UserContext);
   const { colors } = useTheme();
-  const [form, handleFormUpdate] = useForm({ email: '', password: '' })
+  const [form, handleFormUpdate] = useForm({ email: "", password: "" });
   const [showPassword, setShowPassword] = useState(false);
-  const { errorMessage, setErrorMessage } = useError()
-  const [loading, setLoading] = useState(false)
+  const { errorMessage, setErrorMessage } = useError();
+  const [loading, setLoading] = useState(false);
 
   const handleLogin = async () => {
-    setLoading(true)
-    let response = await loginUser(form.email, form.password)
+    setLoading(true);
+    let response = await loginUser(form.email, form.password);
     if (response.error) {
-      setLoading(false)
-      setErrorMessage(response.message)
+      setLoading(false);
+      setErrorMessage(response.message);
     } else {
-      setLoading(false)
-      saveUser(response)
+      setLoading(false);
+      saveUser(response);
     }
-  }
+  };
 
   return (
     <SafeAreaView>
-      <View style={{ backgroundColor: Colors.backgroundColor, paddingHorizontal: 20 }}>
+      <View
+        style={{
+          backgroundColor: Colors.backgroundColor,
+          paddingHorizontal: 20,
+        }}
+      >
         <View
           style={{
-            alignItems: 'center',
-            justifyContent: 'center',
+            alignItems: "center",
+            justifyContent: "center",
             marginTop: 50,
-          }}>
-          <BrightText style={{ fontSize: 25 }}>Login to your account</BrightText>
+          }}
+        >
+          <BrightText style={{ fontSize: 25 }}>
+            Login to your account
+          </BrightText>
         </View>
 
-        <HelperText visible={Boolean(errorMessage)} type="error" style={{ marginTop: 15 }}>{errorMessage}</HelperText>
+        <HelperText
+          visible={Boolean(errorMessage)}
+          type="error"
+          style={{ marginTop: 15 }}
+        >
+          {errorMessage}
+        </HelperText>
         <TextInput
           mode="outlined"
           label="Email"
           left={<TextInput.Icon name="account" />}
           value={form.email}
-          onChangeText={text => {
-            handleFormUpdate('email', text)
-            setErrorMessage('')
+          onChangeText={(text) => {
+            handleFormUpdate("email", text);
+            setErrorMessage("");
           }}
         />
 
@@ -69,39 +84,42 @@ const Login: React.FC<IProps> = ({navigation}) => {
           label="Password"
           secureTextEntry={!showPassword}
           left={<TextInput.Icon name="key" />}
-          right={!showPassword ?
-            <TextInput.Icon
-              name="eye"
-              onPress={() => setShowPassword(true)}
-            />
-            :
-            <TextInput.Icon
-              name="eye-off"
-              onPress={() => setShowPassword(false)}
-            />
+          right={
+            !showPassword ? (
+              <TextInput.Icon
+                name="eye"
+                onPress={() => setShowPassword(true)}
+              />
+            ) : (
+              <TextInput.Icon
+                name="eye-off"
+                onPress={() => setShowPassword(false)}
+              />
+            )
           }
           value={form.password}
-          onChangeText={text => {
-            handleFormUpdate('password', text)
-            setErrorMessage('')
+          onChangeText={(text) => {
+            handleFormUpdate("password", text);
+            setErrorMessage("");
           }}
         />
 
         <View
           style={{
-            flexDirection: 'row',
-            alignItems: 'center',
+            flexDirection: "row",
+            alignItems: "center",
             marginTop: 10,
             marginLeft: 20,
-          }}>
+          }}
+        >
           <MediumText>Need to Register? </MediumText>
-          <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+          <TouchableOpacity onPress={() => navigation.navigate("Register")}>
             <Text style={{ color: colors.primary }}>Register here</Text>
           </TouchableOpacity>
         </View>
 
         <Button
-          style={{ marginTop: 50, height: 40, justifyContent: 'center' }}
+          style={{ marginTop: 50, height: 40, justifyContent: "center" }}
           mode="contained"
           onPress={() => handleLogin()}
           disabled={false}
@@ -109,7 +127,6 @@ const Login: React.FC<IProps> = ({navigation}) => {
         >
           Login
         </Button>
-
       </View>
     </SafeAreaView>
   );
--- src/views/Register/index.tsx
+++ src/views/Register/index.tsx
@@ -1,17 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from "react";
+import { View, TouchableOpacity, SafeAreaView } from "react-native";
+
 import {
-  View,
-  TouchableOpacity,
-  SafeAreaView
-} from 'react-native';
+  TextInput,
+  HelperText,
+  Text,
+  Button,
+  useTheme,
+} from "react-native-paper";
 
-import { TextInput, HelperText, Text, Button, useTheme } from 'react-native-paper';
-
-import { Colors } from '../../styles';
-import { BrightText, MediumText } from '../../components/Texts'
-import { useForm } from '../../hooks/useForm';
-import { updatePassword } from '../../api/updatePassword';
-import { useError } from '../../hooks/useError';
+import { Colors } from "../../styles";
+import { BrightText, MediumText } from "../../components/Texts";
+import { useForm } from "../../hooks/useForm";
+import { updatePassword } from "../../api/updatePassword";
+import { useError } from "../../hooks/useError";
+import { UserContext } from "../../context/UserContext";
 
 interface IProps {
   navigation: any;
@@ -20,54 +23,78 @@ interface IProps {
 
 const Register = (props: IProps) => {
   const { colors } = useTheme();
-  const [form, handleFormUpdate] = useForm({ email: '', password: '', confirmPassword: '', token: '', phoneNumber: '' })
+  const { saveUser } = useContext(UserContext);
+  const [form, handleFormUpdate] = useForm({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    token: "",
+    phoneNumber: "",
+  });
   const [showPassword, setShowPassword] = useState(false);
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
-  const { errorMessage, setErrorMessage } = useError()
-  const [loading, setLoading] = useState(false)
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const { errorMessage, setErrorMessage } = useError();
+  const [loading, setLoading] = useState(false);
 
   const handleRegister = async () => {
-    setLoading(true)
-    let response = await updatePassword(form.email, form.phoneNumber, form.password, form.token);
+    setLoading(true);
+    let response = await updatePassword(
+      form.email,
+      form.phoneNumber,
+      form.password,
+      form.token
+    );
     if (response.error) {
-      setLoading(false)
-      setErrorMessage(response.message)
+      setLoading(false);
+      setErrorMessage(response.message);
     } else {
-      setLoading(false)
-      console.log(response)
+      setLoading(false);
+      saveUser(response);
     }
-  }
+  };
 
   useEffect(() => {
     if (form.confirmPassword && form.confirmPassword !== form.password) {
-      setErrorMessage("Passwords do not match")
+      setErrorMessage("Passwords do not match");
+    } else {
+      setErrorMessage("");
     }
-    else {
-      setErrorMessage('')
-    }
-  }, [form])
+  }, [form]);
 
   return (
     <SafeAreaView>
-      <View style={{ backgroundColor: Colors.backgroundColor, paddingHorizontal: 20 }}>
+      <View
+        style={{
+          backgroundColor: Colors.backgroundColor,
+          paddingHorizontal: 20,
+        }}
+      >
         <View
           style={{
-            alignItems: 'center',
-            justifyContent: 'center',
+            alignItems: "center",
+            justifyContent: "center",
             marginTop: 30,
-          }}>
+          }}
+        >
           <BrightText style={{ fontSize: 25 }}>Register you account</BrightText>
         </View>
 
-        <HelperText type='error' visible={errorMessage && errorMessage.includes('Email') ? true : false} >{errorMessage}</HelperText>
+        <HelperText
+          type="error"
+          visible={
+            errorMessage && errorMessage.includes("Email") ? true : false
+          }
+        >
+          {errorMessage}
+        </HelperText>
         <TextInput
           mode="outlined"
           style={{ marginBottom: 30 }}
           label="Email"
           left={<TextInput.Icon name="account" />}
           value={form.email}
-          onChangeText={text => {
-            handleFormUpdate('email', text)
+          onChangeText={(text) => {
+            handleFormUpdate("email", text);
           }}
         />
 
@@ -77,85 +104,111 @@ const Register = (props: IProps) => {
           label="Password"
           secureTextEntry={!showPassword}
           left={<TextInput.Icon name="key" />}
-          right={!showPassword ?
-            <TextInput.Icon
-              name="eye"
-              onPress={() => setShowPassword(true)}
-            />
-            :
-            <TextInput.Icon
-              name="eye-off"
-              onPress={() => setShowPassword(false)}
-            />
+          right={
+            !showPassword ? (
+              <TextInput.Icon
+                name="eye"
+                onPress={() => setShowPassword(true)}
+              />
+            ) : (
+              <TextInput.Icon
+                name="eye-off"
+                onPress={() => setShowPassword(false)}
+              />
+            )
           }
           value={form.password}
-          onChangeText={text => {
-            handleFormUpdate('password', text)
+          onChangeText={(text) => {
+            handleFormUpdate("password", text);
           }}
         />
 
-        <HelperText type='error' visible={errorMessage && errorMessage.includes('do not match') ? true : false} >{errorMessage}</HelperText>
+        <HelperText
+          type="error"
+          visible={
+            errorMessage && errorMessage.includes("do not match") ? true : false
+          }
+        >
+          {errorMessage}
+        </HelperText>
         <TextInput
           mode="outlined"
           style={{ marginBottom: 15 }}
           label="Confirm Password"
           secureTextEntry={!showConfirmPassword}
           left={<TextInput.Icon name="key" />}
-          right={!showConfirmPassword ?
-            <TextInput.Icon
-              name="eye"
-              onPress={() => setShowConfirmPassword(true)}
-            />
-            :
-            <TextInput.Icon
-              name="eye-off"
-              onPress={() => setShowConfirmPassword(false)}
-            />
+          right={
+            !showConfirmPassword ? (
+              <TextInput.Icon
+                name="eye"
+                onPress={() => setShowConfirmPassword(true)}
+              />
+            ) : (
+              <TextInput.Icon
+                name="eye-off"
+                onPress={() => setShowConfirmPassword(false)}
+              />
+            )
           }
           value={form.confirmPassword}
-          onChangeText={text => {
-            handleFormUpdate('confirmPassword', text)
+          onChangeText={(text) => {
+            handleFormUpdate("confirmPassword", text);
           }}
         />
 
-        <HelperText type='error' visible={errorMessage && errorMessage.includes('phone') ? true : false} >{errorMessage}</HelperText>
+        <HelperText
+          type="error"
+          visible={
+            errorMessage && errorMessage.includes("phone") ? true : false
+          }
+        >
+          {errorMessage}
+        </HelperText>
         <TextInput
           mode="outlined"
           style={{ marginBottom: 15 }}
           label="Phone Number"
           left={<TextInput.Icon name="phone" />}
           value={form.phoneNumber}
-          onChangeText={text => {
-            handleFormUpdate('phoneNumber', text)
+          onChangeText={(text) => {
+            handleFormUpdate("phoneNumber", text);
           }}
         />
 
-        <HelperText type='error' visible={errorMessage && errorMessage.includes('token') ? true : false} >{errorMessage}</HelperText>
+        <HelperText
+          type="error"
+          visible={
+            errorMessage && errorMessage.includes("token") ? true : false
+          }
+        >
+          {errorMessage}
+        </HelperText>
         <TextInput
           mode="outlined"
           label="Token"
           left={<TextInput.Icon name="alpha-t-circle" />}
           value={form.token}
-          onChangeText={text => {
-            handleFormUpdate('token', text)
+          onChangeText={(text) => {
+            handleFormUpdate("token", text);
           }}
         />
 
         <View
           style={{
-            flexDirection: 'row',
-            alignItems: 'center',
+            flexDirection: "row",
+            alignItems: "center",
             marginTop: 10,
             marginLeft: 20,
-          }}>
+          }}
+        >
           <MediumText>Have an Account? </MediumText>
-          <TouchableOpacity onPress={() => props.navigation.navigate('Login')}>
+          <TouchableOpacity onPress={() => props.navigation.navigate("Login")}>
             <Text style={{ color: colors.primary }}>Login here</Text>
           </TouchableOpacity>
         </View>
 
         <Button
-          style={{ marginTop: 50, height: 40, justifyContent: 'center' }}
+          style={{ marginTop: 50, height: 40, justifyContent: "center" }}
           mode="contained"
           onPress={() => handleRegister()}
           disabled={Boolean(errorMessage)}
@@ -163,7 +216,6 @@ const Register = (props: IProps) => {
         >
           Register
         </Button>
-
       </View>
     </SafeAreaView>
   );
