73c224deee3ad11c9c2c00917ad605339d81ad1a
Cooper Holt <cooperholt@Coopers-MacBook-Pro.local>
2022-05-11 17:39:09 -0600

    you can now see debts

--- src/views/DebtsDisplay/DebtsDisplay.tsx
+++ src/views/DebtsDisplay/DebtsDisplay.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { View, Text, FlatList } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Card, List } from "react-native-paper";
+import { IDebt } from "../../types";
 
 interface IProps {
   route: any;
@@ -12,10 +15,25 @@ const DebtsDisplay: React.FC<IProps> = ({ route }) => {
   console.log("debts", debts);
   console.log("totalDebt", totalDebt);
 
-  const renderItem = ({ item }) => <Text>{item.name}</Text>;
+  const renderItem = ({ item }: { item: IDebt }) => (
+    <Card style={{ marginBottom: 10 }}>
+      <List.Item
+        title={item.name}
+        description={`$${item.amountRemaining}`}
+        // right={(props) => (
+        //   <TouchableOpacity>
+        //     <List.Icon icon="delete" />
+        //   </TouchableOpacity>
+        // )}
+        // onPress={() => {
+        //   navigation.navigate("Client", { client: item });
+        // }}
+      />
+    </Card>
+  );
 
   return (
-    <View>
+    <View style={{ flex: 1, padding: 10 }}>
       <FlatList
         data={debts}
         renderItem={renderItem}
